85aab3f0bddfbfa76520a20b3f19e2f92106b4d1
WangZixin67 <60345019+WangZixin67@users.noreply.github.com>
2020-10-11 20:06:39 +0800

    Revert "Varsha version1"

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -5,11 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class DukeTest {
-    static final String LOGO = "    ____        _        \n"
-            + "    |  _ \\ _   _| | _____ \n"
-            + "    | | | | | | | |/ / _ \\\n"
-            + "    | |_| | |_| |   <  __/\n"
-            + "    |____/ \\__,_|_|\\_\\___|\n";
     @Test
     public void sampleTest() {
         assertTrue(true);
